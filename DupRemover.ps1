b7b553aaf4b66c76b63475bb8a3365011e72d78b
mvarela150 <34444725+mvarela150@users.noreply.github.com>
2017-12-11 04:14:59 -0800

    Update DupRemover.ps1

--- DupRemover.ps1
+++ DupRemover.ps1
@@ -4,9 +4,7 @@ $counter = 0
 $substart = 1
 $n = 1 # width of dup. file search
 foreach ($file in $files){
-	#$subset = Get-ChildItem $path -exclude $file | Sort-Object -Property length #w/o indexed file
 	$file.name
-	#$substart
 	foreach($subfile in $files[$substart..($substart+$n)]){
 	"	"+$subfile.name
 		if (((Get-FileHash $path\$file).hash) -ne ((Get-FileHash -Path $path\$subfile).hash)) { #diff hash values
@@ -16,7 +14,6 @@ foreach ($file in $files){
 			### Send to RecycleBin
 			$shell = new-object -comobject "Shell.Application"
 			$item = $shell.Namespace(0).ParseName($path+"\"+$file.name)
-			#$item  -eq $null
 			$item.InvokeVerb("delete")
 			"	match"
 			break
